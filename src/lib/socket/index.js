a50e1fb963f174af7f3423f952b305b7d68fd1f3
yami03 <seula00027@gmail.com>
2019-10-25 11:02:26 +0900

    Remove console

--- src/lib/socket/index.js
+++ src/lib/socket/index.js
@@ -3,25 +3,20 @@ import { MESSAGE, ERROR, JOIN, LEAVE } from '../../contants/socketEventTypes';
 
 export default function() {
   const socket = io.connect('http://localhost:4001');
-  console.log('내가 소켓이다');
 
   socket.on(ERROR, function(err) {
-    console.log('received socket error:');
     console.log(err);
   });
 
   const receiveNewMessage = cb => {
-    console.log('receiveNewMessage');
     socket.on(MESSAGE, cb);
   };
 
   const joinRoom = data => {
-    console.log('join room front');
     socket.emit(JOIN, data);
   };
 
   const sendMessage = data => {
-    console.log(data);
     socket.emit(MESSAGE, data);
   };
 
