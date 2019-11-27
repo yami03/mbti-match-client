1fb0f5e7d9fb8650b6192cebcf8044b02125c243
yami03 <seula00027@gmail.com>
2019-11-27 21:39:49 +0900

    Change uri

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     proxy({
-      target: 'http://localhost:4001',
+      target: 'https://api.slaspace.com',
       changeOrigin: true
     })
   );
