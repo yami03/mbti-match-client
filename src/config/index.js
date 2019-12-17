2c46c34ad837885b011fec56f053cc8a0d23c478
yami03 <seula00027@gmail.com>
2019-12-18 03:15:03 +0900

    Change uri

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
   ROOT = 'http://localhost:8081';
   SOCKET_ROOT = 'http://localhost:8081';
 } else {
-  ROOT = 'http://api.slaspace.com';
+  ROOT = 'https://api.slaspace.com';
   SOCKET_ROOT = 'http://13.125.6.54:8080';
 }
 
